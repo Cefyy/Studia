92a32cf23a3b881662886a2df86105de55a5c91e
Jakub Grzelak <jakubgrzelak84@gmail.com>
2025-10-11 23:23:06 +0200

    Numerki L1/Z6

---
+++ Sem3/Numerki/lista1/z6.cpp
@@ -0,0 +1,25 @@
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    double sumad=0;
+    float sumaf=0;
+
+    for(int i=0;i<=1999999;i++)
+    {
+        if(i%2==0)
+        {
+            sumad+=double(4.0/(2.0*i+1.0));
+            sumaf+=float(4.0/(2.0*i+1.0));
+        }
+        else
+        {
+            sumad-=double(4.0/(2.0*i+1.0));
+            sumaf-=float(4.0/(2.0*i+1.0));
+        }
+    }
+    cout << setprecision(18) <<"roznica dla arytmetyki podwojnej: " << M_PI-sumad << endl << "roznica dla arytmetyki pojedynczej: " << M_PI-sumaf;
+}
