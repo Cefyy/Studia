b0e880cd9abad59b371a5b26b8a5e5c0997d78ab
Jakub Grzelak <jakubgrzelak84@gmail.com>
2025-10-14 12:02:09 +0200

    Rozszerzony Python l2/z5 done

--- Sem3/Python/Lista2/z5.py
+++ Sem3/Python/Lista2/z5.py
@@ -1,14 +1,57 @@
-def kompresja(tekst):
-    tekst = tekst.split
+import sys
+import requests
+
+
+try:
+    sys.stdout.reconfigure(encoding='utf-8')
+except AttributeError:
+    # Python < 3.7
+    import io
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+def scrape_txt(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    response.encoding= 'utf-8'
+    return response.text
+
+def kompresja(tekst : str):
+    tekst = tekst.split()
     wynik=[]
     for word in tekst:
-        for letter in word:
+        length = len(word)
+        licznik=1
+        for i in range(1,length):
+
+            if(word[i]==word[i-1]):
+                licznik+=1
+            else:
+                wynik.append((word[i-1],licznik))
+                licznik=1
+        wynik.append((word[length-1],licznik))
+        wynik.append((' ',1))
+    return wynik
+                    
             
 
 
 
 def dekompresja(tekst_skompresowany):
-    pass
-
-kompresja(" jdsj jfdsjf jdsfjsdfg")
+    decoded=""
+    for krotka in tekst_skompresowany:
+        currletter,currcnt = krotka
+        for i in range(0,currcnt):
+            decoded=decoded+currletter
+    return decoded
+            
+if __name__ == '__main__':
+    url = "https://wolnelektury.pl/media/book/txt/treny-tren-viii.txt"
+    compressedTren=kompresja(scrape_txt(url))
+    decompressedTren=dekompresja(compressedTren)
+    compressed = kompresja("aaab! bbbbccd, dddd!")
+    decompressed = dekompresja(compressed)
+    print(compressedTren)
+    print(decompressedTren)
+    print(compressed)
+    print(decompressed)
 
