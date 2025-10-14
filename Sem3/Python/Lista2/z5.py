034118061a588bc1c9d8f616b8a6969a86b954eb
Jakub Grzelak <jakubgrzelak84@gmail.com>
2025-10-14 11:36:36 +0200

    Roszerzony Python l2/z5 wip

---
+++ Sem3/Python/Lista2/z5.py
@@ -0,0 +1,14 @@
+def kompresja(tekst):
+    tekst = tekst.split
+    wynik=[]
+    for word in tekst:
+        for letter in word:
+            
+
+
+
+def dekompresja(tekst_skompresowany):
+    pass
+
+kompresja(" jdsj jfdsjf jdsfjsdfg")
+
