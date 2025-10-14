39a0b7117cea4af8751ebf71b70dcbb4eb24c33f
Jakub Grzelak <jakubgrzelak84@gmail.com>
2025-10-14 09:24:09 +0200

    poprawka

--- Sem3/Python/Lista2/z4.py
+++ Sem3/Python/Lista2/z4.py
@@ -2,17 +2,22 @@ import random
 import sys
 import requests
 
-
+try:
+    sys.stdout.reconfigure(encoding='utf-8')
+except AttributeError:
+    # Python < 3.7
+    import io
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 def scrape_txt(url):
     response = requests.get(url)
     response.raise_for_status()
     response.encoding= 'utf-8'
     return response.text
 def uprosc_zdanie(tekst: str, dl_slowa: int, liczba_slow: int) -> None:
-
+    
     tokens = tekst.split()
     # Zostawiamy tylko krótsze słowa
-    tokens = [s for s in tokens if len(s) < dl_slowa]
+    tokens = [s for s in tokens if len(s) < dl_slowa and s.isalpha()]
 
     # Jeśli jest więcej słów niż potrzeba, usuń losowo nadmiar
     while len(tokens) > liczba_slow:
