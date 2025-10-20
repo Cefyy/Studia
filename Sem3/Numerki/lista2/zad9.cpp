242fbd8fc1d7fe638b6666e43a975dccf959690a
Jakub Grzelak <jakubgrzelak84@gmail.com>
2025-10-20 21:35:10 +0200

    poprawki

--- Sem3/Numerki/lista2/zad9.cpp
+++ Sem3/Numerki/lista2/zad9.cpp
@@ -1,35 +1,32 @@
-#include <bits/stdc++.h>    
+#include <bits/stdc++.h>
 using namespace std;
 
-double original(double x_k,int k)
+double original(double x_k, int k)
 {
-    double l=x_k/pow(2.0,k);
-    double wyrazenie= 1.0 - sqrt(1.0 - l*l);
-    return pow(2.0,k)*sqrt(2*wyrazenie);
+    double l = x_k / pow(2.0, k);
+    double wyrazenie = 1.0 - sqrt(1.0 - l * l);
+    return pow(2.0, k) * sqrt(2 * wyrazenie);
 }
-double improved(double x_k,int k)
+double improved(double x_k, int k)
 {
-    double l=x_k/pow(2.0,k);
-    double wyrazenie = 1.0 + sqrt(1.0-l*l);
-    return (sqrt(2.0)*x_k)/(sqrt(wyrazenie));
-
+    double l = x_k / pow(2.0, k);
+    double wyrazenie = 1.0 + sqrt(1.0 - l * l);
+    return (sqrt(2.0) * x_k) / (sqrt(wyrazenie));
 }
-
 
 int main()
 {
 
     double x_orig = 2.0;
     double x_improved = 2.0;
-    for(int k = 1;k<=100;k++)
+    for (int k = 1; k <= 150; k++)
     {
-        double next_orig = original(x_orig,k);
-        double next_improved = improved(x_improved,k);
+        double next_orig = original(x_orig, k);
+        double next_improved = improved(x_improved, k);
         cout << fixed << setprecision(16);
         cout << "K : " << k << " " << next_orig << " " << next_improved << endl;
-        
+
         x_orig = next_orig;
         x_improved = next_improved;
-
     }
 }
