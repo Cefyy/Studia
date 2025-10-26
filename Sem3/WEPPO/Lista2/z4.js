4ceda2a4f3ea6e7b5734c6aaadc93b39e3cd8eac
Jakub Grzelak <jakubgrzelak84@gmail.com>
2025-10-26 18:28:28 +0100

    poprawki

---
+++ Sem3/WEPPO/Lista2/z4.js
@@ -0,0 +1,17 @@
+/*typeof zwraca prymitywne typy -> number,string,boolean,object jako string 
+instanceof zwraca boolean czy obiekt został stworzony przed dany konstruktor 
+np. Array, Object, Date itd.
+ale nie działa z prymitywnymi
+*/
+
+console.log(typeof 5)
+console.log(typeof "abc")
+console.log(typeof true)
+console.log(typeof [])
+
+
+console.log([] instanceof Array)
+console.log([] instanceof Object)
+
+console.log(5 instanceof Number)
+console.log(true instanceof Boolean)
