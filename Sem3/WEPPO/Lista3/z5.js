b469d48f1d7965471250c7b25e20067cfa35a5e9
Jakub Grzelak <jakubgrzelak84@gmail.com>
2025-11-02 20:15:03 +0100

    poprawki

--- Sem3/WEPPO/Lista3/z5.js
+++ Sem3/WEPPO/Lista3/z5.js
@@ -14,6 +14,14 @@ var foo = {
 [Symbol.iterator] : function()
 {return createGenerator(10)}
 };
+var foo2 = {
+    [Symbol.iterator] : function()
+    {
+        return createGenerator(42)
+    }
+}
 
 for ( var f of foo )
+console.log(f);
+for ( var f of foo2 )
 console.log(f);
