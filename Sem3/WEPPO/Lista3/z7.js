495b4196a03bc089fd97339af6399dcea4fa8676
Jakub Grzelak <jakubgrzelak84@gmail.com>
2025-11-02 19:59:42 +0100

    Weppo Lista 3/ zad 7

---
+++ Sem3/WEPPO/Lista3/z7.js
@@ -0,0 +1,49 @@
+function createIterator()
+{
+    var f1=0;
+    var f2=1;
+
+    return {
+        next : function()
+        {
+            const value = f2 + f1;
+            var temp = f2;
+            f2 = value;
+            f1 = temp;
+            const done = false
+            return {
+                value,done
+            }
+        }
+    }
+}
+function* createGenerator(){
+   var f1 = 0
+   var f2 = 1
+    yield f2
+   while(true)
+   {
+     var temp = f2
+     f2 = f2+f1
+     f1 = temp
+     yield f2
+   }
+}
+it = createIterator()
+it1 = createGenerator()
+function* take(it,top)
+{
+    while(top)
+    {
+        top--
+        yield it.next()
+    }
+}
+
+for (var i of take(it,10)) {
+    console.log(i.value);
+}
+for(var i of take(it1,10))
+{
+    console.log(i.value)
+}
