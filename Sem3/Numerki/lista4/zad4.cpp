83e2b5d1e95d1883ad7ff55720af244a0179a94b
Jakub Grzelak <jakubgrzelak84@gmail.com>
2025-11-03 14:51:00 +0100

    Numerki Lista 4/zad 4

---
+++ Sem3/Numerki/lista4/zad4.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+//https://www.desmos.com/calculator?lang=pl 3x^{2}-5\cdot\cos\left(7x-1\right)
+using namespace std;
+double funkcja(double x){
+    return 3.0*x*x - 5.0*cos(7.0*x-1);
+}
+
+double bisekcja(double a0, double b0) {
+    double a = a0, b = b0, m;
+
+
+    for (int i = 0; i < 19; ++i) {
+        m = 0.5 * (a + b);
+
+        // warunek stopu: szerokość przedziału mniejsza niż tolerancja
+
+        // wybór podprzedziału
+        if (funkcja(a) * funkcja(m) <= 0)
+            b = m;
+        else
+            a = m;
+    }
+
+    return 0.5 * (a + b);
+}
+
+int main()
+{
+    //z wykresu wychodzi 6 zer
+    cout << setprecision(16)
+    << bisekcja(-1.3,-0.60) << endl
+     << bisekcja(-0.9,-0.2) << endl
+     << bisekcja(-0.5,0.3) << endl
+     << bisekcja(0,0.8) << endl
+     << bisekcja(0.4,1.1) << endl
+     << bisekcja(0.9,1.7);
+}
