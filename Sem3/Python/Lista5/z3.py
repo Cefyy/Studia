d055258ce6c857cd01d4792809d5761183840ac4
Jakub Grzelak <jakubgrzelak84@gmail.com>
2025-11-05 17:32:48 +0100

    python Lista 5/zad 3 poprawki

--- Sem3/Python/Lista5/z3.py
+++ Sem3/Python/Lista5/z3.py
@@ -1,21 +1,21 @@
 import requests
-from bs4 import BeautifulSoup
-from collections import defaultdict,Counter
 import re
+from collections import defaultdict,Counter
+from bs4 import BeautifulSoup
 
 urls = ["https://www.geeksforgeeks.org/python/response-text-python-requests/",
         "https://www.geeksforgeeks.org/python/python-counter-objects-elements/",
         "https://docs.python.org/3/"]
 def scrape_text(url):
     try:
-        html = requests.get(url,timeout=5)
+        html = requests.get(url)
         html.raise_for_status()
-        soup = BeautifulSoup(html.text,features="html.parser")
-        return soup.get_text(separator= " ")
+        soup = BeautifulSoup(html.text,features='html.parser')
+        return soup.get_text(separator=" ")
     except Exception as e:
-        print(f"Error while scraping {url}: {e}")
+        print("Error {e} while scraping {url}")
         return ""
-
+        
 def build_index(urls):
     index = defaultdict(lambda: defaultdict(int))
     for url in urls:
@@ -23,32 +23,25 @@ def build_index(urls):
         words = re.findall(r"\w+",text.lower())
         counter = Counter(words)
         for word,count in counter.items():
-            index[word][url] += count
-            
+            index[word][url]+=count
     return index
-
-
-def most_frequent_word(index):
-    counters = { word: sum(counts.values()) for word,counts in index.items()}
-    word = max(counters,key=counters.get)
-    return word,counters.get(word)
-
-def find_where(index,word):
+def most_common_word(index):
+    count_all = {word : sum(counts.values()) for word,counts in index.items()}
+    word = max(count_all,key = count_all.get)
+    return word,count_all.get(word)
+def find_word(word,index):
     if word not in index:
-        return []
-    return list(index[word].keys())
+        return ""
+    else:
+        return list(index[word].keys())
+
 index = build_index(urls)
-word,count = most_frequent_word(index)
-print(f"Najpopularniejsze słowo: '{word}' ({count} wystąpień)")
-print(f"Występuje na stronach:")
-for url in find_where(index, word):
-    print(" -", url)
-word1="tutorial"
-print(f"Slowo {word1} znajduje sie na: ")
-for url in find_where(index, word1):
-    print(" -",url)
-word1="geeksforgeeks"
-print(f"Slowo {word1} znajduje sie na: ")
-for url in find_where(index, word1):
-    print(" -",url)
-    
+
+word,count = most_common_word(index)
+print(f"The most common word in our list of adresses is {word} with {count} occurences")
+
+found_urls = find_word(word,index)
+print(f"The word {word} is found in these urls: ")
+for url in found_urls:
+    print(url)
+
