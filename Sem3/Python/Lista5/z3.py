196296808dc992b44369165601bec60af0d7b620
Jakub Grzelak <jakubgrzelak84@gmail.com>
2025-11-06 21:31:51 +0100

    python lista5/zad 3 final v

--- Sem3/Python/Lista5/z3.py
+++ Sem3/Python/Lista5/z3.py
@@ -5,7 +5,9 @@ from bs4 import BeautifulSoup
 
 urls = ["https://www.geeksforgeeks.org/python/response-text-python-requests/",
         "https://www.geeksforgeeks.org/python/python-counter-objects-elements/",
-        "https://docs.python.org/3/"]
+        "https://docs.python.org/3/",
+        "https://ii.uni.wroc.pl//"]
+
 def scrape_text(url):
     try:
         html = requests.get(url)
@@ -13,28 +15,30 @@ def scrape_text(url):
         soup = BeautifulSoup(html.text,features='html.parser')
         return soup.get_text(separator=" ")
     except Exception as e:
-        print("Error {e} while scraping {url}")
+        print(f"Error {e} while scraping {url}")
         return ""
         
 def build_index(urls):
     index = defaultdict(lambda: defaultdict(int))
     for url in urls:
         text = scrape_text(url)
-        words = re.findall(r"\w+",text.lower())
+        words = re.findall(r"[A-Za-zÀ-ÿ]+(?:['-][A-Za-zÀ-ÿ]+)*", text.lower())
         counter = Counter(words)
         for word,count in counter.items():
             index[word][url]+=count
     return index
+
 def most_common_word(index):
     count_all = {word : sum(counts.values()) for word,counts in index.items()}
     word = max(count_all,key = count_all.get)
     return word,count_all.get(word)
+
 def find_word(word,index):
     if word not in index:
         return ""
     else:
         return list(index[word].keys())
-
+#======================================================================================#
 index = build_index(urls)
 
 word,count = most_common_word(index)
