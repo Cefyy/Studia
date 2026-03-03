ae1b6ba716de0b1bb2f8ee6556fcde949ff54866
Jakub Grzelak <jakubgrzelak84@gmail.com>
2026-03-03 19:58:16 +0100

    fix

--- Sem4/React/my-business-card/src/App.tsx
+++ Sem4/React/my-business-card/src/App.tsx
@@ -1,17 +1,17 @@
 import ProfileCard from './components/ProfileCard/ProfileCard'
 import type { UserProfile } from './components/types/user'
-import avatarImage from './assets/istockphoto-1051202402-612x612.jpg'
+
 
 
 
 const mockUser : UserProfile = {
-  avatar : avatarImage,
+  avatar : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLVdh-YHZWYakHXc-MA9L9Seau-WTmkU7kEw&s",
   name: 'John Doe',
   title: 'Software Engineer',
   company: 'ABC LLC',
   contact: {
     phone: '+48 123 456 789',
-    email: 'john.does@example.com',
+    email: 'john.doe@example.com',
     website: 'www.johndoe.dev'
   },
   about: 'Passionate engineer with over 8 years of experience in building scalable web applications. I specialize in React ecosystems and performance optimization, always focusing on delivering clean, maintainable code and exceptional user experiences.',
